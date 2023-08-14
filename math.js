ecf6d18601e69d36acc8d056c59b921abad1e468
NIKHILS\nikhils <nikhil.s@codecraft.co.in>
2023-08-14 15:10:42 +0530

    fix: fixed syntax error

--- math.js
+++ math.js
@@ -7,12 +7,14 @@
 function add(num1,num2)
 {
     return num1+num2;
-}/**
+}
+
+/**
  * This function calculates and returns the difference of two numbers
-* @param {number} number1 
+ * @param {number} number1 
  * @param {number} number2 
  * @returns result of number1 - number2
  */
 function subtract(number1, number2){
     return number1 - number2;
-}/**
+}
