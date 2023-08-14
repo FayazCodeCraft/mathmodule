ad091eec1bb4a40a269819789c470142684d7a2d
vinusuhas-codecraft <vinu.dlima@codecraft.co.in>
2023-08-14 14:38:28 +0530

    feat: power function added

---
+++ math.js
@@ -0,0 +1,9 @@
+/**
+ * number and power are the input and it will return the output
+ * @param {*} number
+ * @param {*} power
+ * @returns 
+ */
+function powers(number,power){
+    return number**power
+}
