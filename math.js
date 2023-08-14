5142c48505652343780d126db1c41fefa1a1d52b
NIKHILS\nikhils <nikhil.s@codecraft.co.in>
2023-08-14 14:29:08 +0530

    feat: added subtract function
    feat:implemented add function

---
+++ math.js
@@ -0,0 +1,18 @@
+/**
+ * This function calculates and returns the difference of two numbers
+* @param {number} number1 
+ * @param {number} number2 
+ * @returns result of number1 - number2
+ */
+function subtract(number1, number2){
+    return number1 - number2;
+}/**
+ * Calcute and return the addition of two numbers
+ * @param {number} num1 
+ * @param {number} num2 
+ * @returns the result 
+ */
+function add(num1,num2)
+{
+    return num1+num2;
+}
